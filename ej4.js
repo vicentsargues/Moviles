7d7777c135988865f196dd170e30fcc992dac46d
vicentsargues <vicentsargues@gmail.com>
2020-09-24 17:49:38 +0200

    ejerciocio4

---
+++ ej4.js
@@ -0,0 +1,25 @@
+var numero= 5;
+
+var numero2 = 8;
+
+if(numero<=numero2) {
+
+console.log ("número no es mayor que numero2");
+
+}
+
+if(numero2>0) {
+
+console.log ("número 2 es positivo");
+
+}
+
+if(numero<0) {
+    console.log ("número es negativo o distinto de cero");
+}
+
+if((numero+1)<=numero2){
+
+console.log("Incrementar en 1 unidad el valor de numero no lo hace mayoro igual que numero2");
+
+}
